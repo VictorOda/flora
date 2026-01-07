f5731a50f349873edc9da1d958ca4b0d1e0a907c
Victor Oda <VictorOda@users.noreply.github.com>
2026-01-07 19:09:13 -0300

    fix: db migrations
    Supabase was handling the migrations, I switched so drizzle will handle them instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
+import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
 export default defineConfig({
-  out: './supabase/migrations',
+  out: './src/db/migrations',
   schema: './src/db/schema.public.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
   schemaFilter: ['public'],
   verbose: true,
